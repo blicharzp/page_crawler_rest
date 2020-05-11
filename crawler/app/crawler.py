0dcf4c1a5d2f0b7b2f4ee564450786aa12f1f605
Pawel Blicharz <pawelblicharz@gmail.com>
2020-05-11 16:56:06 +0200

    Rewritten crawler service to use asyncio and aiohttp libraries

--- crawler/app/crawler.py
+++ crawler/app/crawler.py
@@ -1,61 +1,56 @@
+import aiohttp
+import asyncio
+import json
+import os
 from bs4 import BeautifulSoup
-import requests
-import threading, queue, time, json, os
 
-STORAGE_ADDRESS = "http://{address}:{port}/{api}".format(
+STORAGE_ADDRESS="http://{address}:{port}/{api}".format(
     address=os.getenv("STORAGE_SERVICE_NAME"),
     port=os.getenv("STORAGE_SERVICE_PORT"),
     api="api/v1/content/"
 )
 
-
-class Crawler:
-    def __init__(self, link="https://www.gazeta.pl/0,0.html"):
-        self.__link = link
-        self.__queue = queue.Queue()
-
-    def produce(self):
-        print("Started produce job.")
-        while True:
-            page = requests.get(self.__link, verify=False)
-            soup =  BeautifulSoup(page.content, features="html.parser")
-            self.__crawle(soup)
-            time.sleep(10 * 60)
-
-    def __crawle(self, soup):
-        for tag in soup.find_all("a", id="LinkArea:BoxOpLink"):
-            name = tag.attrs.get("title", "Title missing")
-            timestamp = tag.find("span", class_="o-article__timestamp is-new").string
-            content = {'name': name, 'timestamp': timestamp}
-            print("Producer - put: {}".format(content))
-            self.__queue.put(content)
-
-    def consume(self):
-        print("Started consume job.")
-        while True:
-            try:
-                content = self.__queue.get()
-            except queue.Empty:
-                print("Consumer - nothing to send")
-            else:
-                self.__send(content)
-            finally:
-                time.sleep(5)
-
-    def __send(self, content):
-        try:
-            requests.post(STORAGE_ADDRESS, json=json.dumps(content))
-            print("Consumer - send {}".format(content))
-        except requests.exceptions.ConnectionError:
-            print("Consumer - cannot establish connection")
+SOURCE_ADDRESS="{}".format(os.getenv("CRAWLER_SOURCE_ADDRESS"))
 
 
-def main():
-    crawler = Crawler()
-    threading.Thread(target = crawler.produce).start()
-    threading.Thread(target = crawler.consume).start()
+async def fetch(session, url):
+    async with session.get(url) as response:
+        page = await response.read()
+        async for content in crawle(BeautifulSoup(page, 'html.parser')):
+            yield content
 
 
+async def crawle(soup):
+    for tag in soup.find_all("a", id="LinkArea:BoxOpLink"):
+        name = tag.attrs.get("title", "Title missing")
+        timestamp = tag.find("span", class_="o-article__timestamp is-new").string
+        content = {'name': name, 'timestamp': timestamp}
+        print("crawle: put: {}".format(content))
+        yield content
 
-if __name__ == "__main__":
-    main()
+
+async def send(session, content):
+    try:
+        await session.post(STORAGE_ADDRESS, json=json.dumps(content))
+        print("send: {} sending...".format(content))
+    except aiohttp.client_exceptions.ClientConnectorError:
+        print("send: cannot establish connection")
+    finally:
+        await asyncio.sleep(1)
+
+
+async def loop():
+    async with aiohttp.ClientSession() as session:
+        print(SOURCE_ADDRESS)
+        async for content in fetch(session, SOURCE_ADDRESS):
+            await send(session, content)
+
+
+async def main():
+    while True:
+        await loop()
+        await asyncio.sleep(10 * 60) # 10 minutes
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
